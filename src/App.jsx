23073fea3b2c26d3aba23c4ce3096ec16048afc0
Sreejith S Kumar <sreejithskumar80@gmail.com>
2022-09-09 13:12:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { ListingPage } from './Pages/ListingPage/LisitngPage';
+
+function App() {
+  return (
+   <>
+   <ListingPage/>
+   </>
+  );
+}
+
+export default App;
---
+++ src/Pages/ListingPage/LisitngPage.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { useState } from "react";
+import ListingPageStyles from "./LisitngPage.module.css";
+
+
+export function ListingPage()
+{
+const [data,setData]=useState([]);
+    let outlets=[
+        {   "id":1,
+            "name":"Outlet 1",
+            "location":"1km away from your location",
+            "vehicles":[
+                {
+                    "id":1,
+                    "vehicle_name":"Activa",
+                    "price":300
+                },
+                {
+                    "id":2,
+                    "vehicle_name":"Activa",
+                    "price":300
+                },
+                {
+                    "id":3,
+                    "vehicle_name":"Activa",
+                    "price":300
+                },
+        ]
+        }
+    ]
+    React.useEffect(()=>
+    {
+        setData(outlets);
+    },[]);
+    return (
+        <div className={ListingPageStyles.Container}>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name of the outlet</th>
+                        <th>Location</th>
+                        <th>Checkout Link</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.map((item)=>
+                        (
+                            <tr>
+                                <td>{item.name}</td>
+                                <td>{item.location}</td>
+                                <td><button>Checkout</button></td>
+                            </tr>
+                            
+                        ))
+                    }
+                    <tr></tr>
+                </tbody>
+            </table>
+
+        </div>
+    )
+}
